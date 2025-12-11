95d0371d1a238ca30d83ce1b2ec7c085e37747ad
Ramyakrishnapitaka <154577647+Ramyakrishnapitaka@users.noreply.github.com>
2025-12-11 16:41:47 +0530

    Uncomment App component and update API URL

--- Task-frontend/src/App.jsx
+++ Task-frontend/src/App.jsx
@@ -1,52 +1,3 @@
-// import { useState, useEffect } from "react";
-// import TaskList from "./components/tasklist.jsx";
-// import Updatetask from "./components/updatetask.jsx";
-// import AddTask from "./components/task.jsx";
-// import "./App.css";
-
-// function App() {
-//   const [tasks, setTasks] = useState([]);
-//   const [editingTask, setEditingTask] = useState(null);
-
-//   const fetchTasks = async () => {
-//     const res = await fetch("http://localhost:8976/api/task");
-//     const data = await res.json();
-//     setTasks(data);
-//   };
-
-//   useEffect(() => {
-//     fetchTasks();
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Task Tracker</h1>
-
-//       <AddTask onTaskAdded={fetchTasks} />
-
-//       {editingTask && (
-//         <Updatetask
-//           task={editingTask}
-//           onUpdateSuccess={() => {
-//             setEditingTask(null);
-//             fetchTasks();
-//           }}
-//         />
-//       )}
-
-//       <TaskList
-//         tasks={tasks}
-//         fetchTasks={fetchTasks}
-//         setEditingTask={setEditingTask}
-//       />
-//     </div>
-//   );
-// }
-
-// export default App;
-   
-
-
 import { useState, useEffect } from "react";
 import TaskList from "./components/tasklist.jsx";
 import Updatetask from "./components/updatetask.jsx";
@@ -58,7 +9,7 @@ function App() {
   const [editingTask, setEditingTask] = useState(null);
 
   const fetchTasks = async () => {
-    const res = await fetch("http://localhost:8976/api/task");
+    const res = await fetch("https://task-tracker-app-0010.onrender.com/api/task");
     const data = await res.json();
     setTasks(data);
   };
